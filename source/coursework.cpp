03da2b63d6d0b5ae9b4f550446746a2f58d3276e
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2025-05-15 15:49:19 +0100

    light source workings

--- source/coursework.cpp
+++ source/coursework.cpp
@@ -103,7 +103,6 @@ int main(void)
 
     // Load the textures
     spooky_cauldron.addTexture("../assets/blue.bmp", "diffuse");
-    plane.addTexture("../assets/bricks_diffuse", "diffuse");
 
     // Use wireframe rendering (comment out to turn off)
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -112,10 +111,10 @@ int main(void)
     //Object object;
 
     // cauldron lighting properties
-    spooky_cauldron.ka = 0.2f; //ambient lighting
+    spooky_cauldron.ka = 0.1f; //ambient lighting
     spooky_cauldron.kd = 0.7f; //diffuse 
     spooky_cauldron.ks = 1.0f; //specular constant
-    spooky_cauldron.Ns = 20.0f; //specular exponent
+    spooky_cauldron.Ns = 2.0f; //specular exponent
 
     //attenuation properties
     float constant = 1.0f;
@@ -126,6 +125,33 @@ int main(void)
     glm::vec3 lightPosition = glm::vec3(2.0f, 2.0f, 2.0f);
     glm::vec3 lightColour = glm::vec3(1.0f, 1.0f, 1.0f);
 
+
+    //spooky cauldron positions
+    glm::vec3 positions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -10.0f),
+        glm::vec3(-3.0f, -2.0f, -3.0f),
+        glm::vec3(-4.0f, -2.0f, -8.0f),
+        glm::vec3(2.0f,  2.0f, -6.0f),
+        glm::vec3(-4.0f,  3.0f, -8.0f),
+        glm::vec3(0.0f, -2.0f, -5.0f),
+        glm::vec3(4.0f,  2.0f, -4.0f),
+        glm::vec3(2.0f,  0.0f, -2.0f),
+        glm::vec3(-1.0f,  1.0f, -2.0f)
+    };
+
+    //add spooky cauldrons to vector
+    std::vector<Object> objects;
+    Object object;
+    object.name = "cauldron";
+    for (unsigned int i = 0; i < 10; i++)
+    {
+        object.position = positions[i];
+        object.rotation = glm::vec3(1.0f, 1.0f, 1.0f);
+        object.scale = glm::vec3(0.75f, 0.75f, 0.75f);
+        object.angle = Maths::radians(20.0f * i);
+        objects.push_back(object);
+    }
 
     // Render loop
     while (!glfwWindowShouldClose(window))
@@ -146,34 +172,11 @@ int main(void)
         // Activate shader
         glUseProgram(shaderID);
 
-        //                          MATRICES    
-
-                // Calculate view and projection matrices
-        camera.target = camera.eye + camera.front;
-        camera.calculateMatrices();
-
-        // Calculate the model matrix
-        glm::mat4 translate;
-        glm::mat4 scale;
-        glm::mat4 rotate;
-        glm::mat4 model = translate * rotate * scale;
-
-        // Calculate the MVP matrix
-        glm::mat4 MVP = camera.projection * camera.view * model;
-
-        // Send MVP matrix to the vertex shader
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
-
-        // Send MV matrix to the vertex shader
-        glm::mat4 MV = camera.view * model;
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
-
-
         //                          SHADER PROPERTIES
 
-                //send properties to shader
+        //send properties to shader
 
-                //ambient
+        //ambient
         glUniform1f(glGetUniformLocation(shaderID, "ka"), spooky_cauldron.ka);
 
         //diffuse
@@ -191,8 +194,64 @@ int main(void)
         glUniform1f(glGetUniformLocation(shaderID, "linear"), linear);
         glUniform1f(glGetUniformLocation(shaderID, "quadratic"), quadratic);
 
-        //// Draw the witches super spooky cauldron
-        spooky_cauldron.draw(shaderID);
+        //                          MATRICES    
+
+                // Calculate view and projection matrices
+        camera.target = camera.eye + camera.front;
+        camera.calculateMatrices();
+
+        // Calculate the model matrix
+        //glm::mat4 translate;
+        //glm::mat4 scale;
+        //glm::mat4 rotate;
+        //glm::mat4 model = translate * rotate * scale;
+
+        // Calculate the MVP matrix
+        //glm::mat4 MVP = camera.projection * camera.view * model;
+
+        // Send MVP matrix to the vertex shader
+        //glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+
+        // Send MV matrix to the vertex shader
+        //glm::mat4 MV = camera.view * model;
+        //glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
+
+        // Loop through objects
+        for (int i = 0; i < static_cast<unsigned int>(objects.size()); i++)
+        {
+            // Calculate model matrix
+            glm::mat4 translate = Maths::translate(objects[i].position);
+            glm::mat4 scale = Maths::scale(objects[i].scale);
+            glm::mat4 rotate = Maths::rotate(objects[i].angle, objects[i].rotation);
+            glm::mat4 model = translate * rotate * scale;
+
+            // Send the MVP and MV matrices to the vertex shader
+            glm::mat4 MV = camera.view * model;
+            glm::mat4 MVP = camera.projection * MV;
+            glUniformMatrix4fv(glGetUniformLocation(shaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+            glUniformMatrix4fv(glGetUniformLocation(shaderID, "MV"), 1, GL_FALSE, &MV[0][0]);
+
+            // Draw the model
+            spooky_cauldron.draw(shaderID);
+        }
+
+
+        glUseProgram(lightShaderID);
+
+        // Calculate model matrix
+        glm::mat4 translate = Maths::translate(lightPosition);
+        glm::mat4 scale = Maths::scale(glm::vec3(0.1f));
+        glm::mat4 model = translate * scale;
+
+        // Send the MVP and MV matrices to the vertex shader
+        glm::mat4 MVP = camera.projection * camera.view * model;
+        glUniformMatrix4fv(glGetUniformLocation(lightShaderID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+
+        // Send model, view, projection matrices and light colour to light shader
+        glUniform3fv(glGetUniformLocation(lightShaderID, "lightColour"), 1, &lightColour[0]);
+
+        // Draw light source
+        sphere.draw(lightShaderID);
 
         // Swap buffers
         glfwSwapBuffers(window);
